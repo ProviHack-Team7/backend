896a1e5d3e615f8dce74218fd9c0f6c7f39c44b0
laisdejesus <dejesusll@outlook.com>
2021-11-28 04:18:26 -0300

    types e tabelas

---
+++ types.ts
@@ -0,0 +1,22 @@
+export type teacherUser = {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+}
+
+ 
+export type material = {
+    id: string,
+    title: string,
+    description: string,
+    url_link: string,
+}
+
+export type quizz = {
+    id: string,
+    question: string,
+    incorrect_1: string,
+    incorrect_2: string,
+    incorrect_3: string,
+}
